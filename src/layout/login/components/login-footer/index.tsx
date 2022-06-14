432c79719e923aefbc67d2193089aa91248e4672
Darkisda <luandesouzanascimento2011@gmail.com>
2022-06-14 10:02:56 -0300

    fix: login footer

--- src/layout/login/components/login-footer/index.tsx
+++ src/layout/login/components/login-footer/index.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function LoginFooter() {
   return(
-    <div className="helpfull-links mt-28 w-9/12 flex justify-around items-center content-center">
+    <div className="helpfull-links mt-24 w-full flex justify-between items-center content-center">
       <Link to="/visite">Visitar site Plantus</Link>
       <Link to="/termos">Termos de Uso</Link>
     </div>
