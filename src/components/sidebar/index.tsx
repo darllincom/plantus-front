4b6d1042a6337ce3a76563c56a7e761f0beab0ec
Darkisda <luandesouzanascimento2011@gmail.com>
2022-06-14 11:21:57 -0300

    chore: welcome: 1280px

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -4,16 +4,21 @@ import { Link } from 'react-router-dom'
 
 export default function Sidebar() {
   return (
-    <aside className="z-50 top-0 fixed left-0 h-screen w-40 flex items-center justify-start content-center flex-col px-4 bg-dark-white ">
+    <aside 
+      className="
+        z-50 top-0 fixed left-0 h-screen w-40 
+        flex items-center justify-start content-center flex-col px-4 bg-dark-white
+        xl:w-2/12
+      ">
       <div className="flex items-start flex-col content-center justify-center mt-12">
-				<img src={Logo} alt="Plantus Logo" className="w-36" />
-				<img src={Grad} alt="Gradiente" className="w-36 mt-2" />
+				<img src={Logo} alt="Plantus Logo" className="w-36 xl:w-40" />
+				<img src={Grad} alt="Gradiente" className="w-36 xl:w-40 mt-2 xl:mt-3" />
 			</div>
-      <div className="flex items-center justify-start content-center  flex-col w-full h-full mt-5">
-        <Link to="/" className="bg-white w-16 h-16 text-xs flex align-center content-center justify-center flex-col rounded-2xl text-center shadow-sm my-3">Inicio</Link>
-        <Link to="/perfil" className="bg-white w-16 h-16 text-xs flex align-center content-center justify-center flex-col rounded-2xl text-center shadow-sm my-3">Perfil</Link>
-        <Link to="/equipe" className="bg-white w-16 h-16 text-xs flex align-center content-center justify-center flex-col rounded-2xl text-center shadow-sm my-3">Equipe</Link>
-        <Link to="/produtos" className="bg-white w-16 h-16 text-xs flex align-center content-center justify-center flex-col rounded-2xl text-center shadow-sm my-3">Produtos</Link>
+      <div className="flex items-center justify-start content-center flex-col w-full h-full mt-5 xl:mt-8">
+        <Link to="/" className="bg-white w-20 h-20 px-2 text-xs flex align-center content-center justify-center flex-col rounded-2xl text-center shadow-sm my-2 xl:w-24 xl:h-24 xl:text-base">Inicio</Link>
+        <Link to="/perfil" className="bg-white w-20 h-20 px-2 text-xs flex align-center content-center justify-center flex-col rounded-2xl text-center shadow-sm my-2 xl:w-24 xl:h-24 xl:text-base">Perfil</Link>
+        <Link to="/equipe" className="bg-white w-20 h-20 px-2 text-xs flex align-center content-center justify-center flex-col rounded-2xl text-center shadow-sm my-2 xl:w-24 xl:h-24 xl:text-base">Equipe</Link>
+        <Link to="/produtos" className="bg-white w-20 h-20 px-2 text-xs flex align-center content-center justify-center flex-col rounded-2xl text-center shadow-sm my-2 xl:w-24 xl:h-24 xl:text-base">Produtos</Link>
       </div>
     </aside>
   )
--- src/templates/welcome/index.tsx
+++ src/templates/welcome/index.tsx
@@ -17,22 +17,22 @@ export default function Welcome() {
         max-w-none mx-auto min-h-screen
       ">
       <Sidebar />
-      <main className="container fixed right-0 h-screen flex flex-row justify-between w-5/6 max-w-none">
+      <main className="container fixed right-0 h-screen flex flex-row justify-between w-5/6 max-w-none xl:w-10/12">
         <section className="w-9/12 px-6">
-          <header className="mt-14">
-            <h1 className="text-dark-green text-xl">Bem vindo à Plataforma Plantus</h1>
+          <header className="mt-14 xl:mt-16">
+            <h1 className="text-dark-green text-xl xl:text-2xl">Bem vindo à Plataforma Plantus</h1>
           </header>
           <section className="flex items-start content-center justify-center flex-row h-full">
-            <div className="w-1/2 mt-14">
+            <div className="w-1/2 mt-44">
               <img src={WelcomeImg} className="w-5/6" alt="Folhas nas cores da Plantus" />
             </div>
-            <div className="w-1/2 mt-14">
-              <p className="text-lg font-light text-dark-gray">
+            <div className="w-1/2 mt-44">
+              <p className="text-lg font-light text-dark-gray xl:text-xl">
                 Aguardando informações da gestão
                 para a página inicial.
               </p>
               <br />
-              <p className="text-lg font-light text-dark-gray">
+              <p className="text-lg font-light text-dark-gray xl:text-xl">
                 Visualize as outras funcionalidades 
                 no menu lateral da Plataforma Plantus!
               </p>
@@ -41,16 +41,16 @@ export default function Welcome() {
         </section>
         <aside className="h-screen px-2 py-10 bg-white w-3/12 right-0 top-0 shadow-md">
           <header className="flex w-full items-center justify-around">
-            <img className="rounded-full w-10" src={user?.image} alt="Avatar do Usuário" />
+            <img className="rounded-full w-10 xl:w-14" src={user?.image} alt="Avatar do Usuário" />
             <section>
-              <p className="text-base-green text-xs">{ user?.fullName }</p>
-              <p className="text-xs">{user?.office}</p>
+              <p className="text-base-green text-xs xl:text-lg">{ user?.fullName }</p>
+              <p className="text-xs xl:text-base">{user?.office}</p>
             </section>
-            <button onClick={handleLogout} className="bg-light-green py-1 px-3 rounded-lg text-sm">Sair</button>
+            <button onClick={handleLogout} className="bg-light-green py-1 px-3 rounded-lg text-sm xl:text-base">Sair</button>
           </header>
         </aside>
       </main>
-      <footer className="fixed bottom-0 right-0 w-11/12 h-12 bg-white">   
+      <footer className="fixed bottom-0 right-0 w-10/12 h-12 bg-white">   
       </footer>
     </div>
   )
