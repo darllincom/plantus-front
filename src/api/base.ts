4968ba31b6be23fe02500c4d42d35357fa2f4299
Darkisda <luandesouzanascimento2011@gmail.com>
2022-06-28 16:33:44 -0300

    fix: fix api token

--- src/api/base.ts
+++ src/api/base.ts
@@ -1,12 +1,5 @@
 import axios from 'axios';
 
-let token = '';
-
-if (typeof window !== 'undefined') {
-	token = localStorage.getItem('accessToken') || '';
-}
-
 export const api = axios.create({
 	baseURL: import.meta.env.VITE_BASE_URL,
-	headers: { Authorization: 'Bearer ' + token }
 });
