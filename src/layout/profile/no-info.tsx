653e4613e5aafbb703e9f06b05e4ffe4bc0c61bc
Darkisda <luandesouzanascimento2011@gmail.com>
2022-07-06 11:41:42 -0300

    enhancement: warnning and popup success

--- src/layout/profile/no-info.tsx
+++ src/layout/profile/no-info.tsx
@@ -10,13 +10,14 @@ export default function NoInfo({ name }: NoInfoProps) {
 
 	return (
 		<div className="w-full h-full flex items-center content-center justify-items-center justify-center flex-col">
-			<h1>Este usuário possui pendências de {name}</h1>
+			{/* <h1>Este usuário possui pendências de {name}</h1>
 			<button
 				onClick={handleEdition}
 				className="px-2 py-1 rounded-md bg-base-green text-xs mt-5 text-white lg:text-lg lg:px-4"
 			>
 				Adicionar Informações
-			</button>
+			</button> */}
+      Em breve
 		</div>
 	);
 }
